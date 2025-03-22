a21f1371af0ec3e26f30a2b6bc20cffd9f7ab912
N-Hagemeister <nicolehagemeister99@gmail.com>
2025-03-22 01:47:50 -0300

    Add files via upload

---
+++ Atividade 04/PTriangulo/Triangulo/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Triangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtA = new System.Windows.Forms.MaskedTextBox();
+            this.txtB = new System.Windows.Forms.MaskedTextBox();
+            this.txtC = new System.Windows.Forms.MaskedTextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(178, 78);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(143, 22);
+            this.txtA.TabIndex = 0;
+            this.txtA.Validated += new System.EventHandler(this.txtA_Validated);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(178, 139);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(143, 22);
+            this.txtB.TabIndex = 1;
+            this.txtB.Validated += new System.EventHandler(this.txtB_Validated);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(178, 208);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(143, 22);
+            this.txtC.TabIndex = 2;
+            this.txtC.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtC_MaskInputRejected);
+            this.txtC.Validated += new System.EventHandler(this.txtC_Validated);
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(62, 78);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(16, 16);
+            this.lblA.TabIndex = 3;
+            this.lblA.Text = "A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(62, 139);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(16, 16);
+            this.lblB.TabIndex = 4;
+            this.lblB.Text = "B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(62, 208);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(16, 16);
+            this.lblC.TabIndex = 5;
+            this.lblC.Text = "C";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(97, 313);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(158, 83);
+            this.btnCalc.TabIndex = 6;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(319, 313);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(158, 83);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(535, 313);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(158, 83);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(365, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(328, 165);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 451);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox txtA;
+        private System.Windows.Forms.MaskedTextBox txtB;
+        private System.Windows.Forms.MaskedTextBox txtC;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Atividade 04/PTriangulo/Triangulo/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangulo
+{
+
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtA.Text = string.Empty;
+            txtB.Text = string.Empty;
+            txtC.Text = string.Empty;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double A = Convert.ToDouble(txtA.Text);
+            double B = Convert.ToDouble(txtB.Text);
+            double C = Convert.ToDouble(txtC.Text);
+
+            if (A + B > C && A + C > B && B + C > A)
+            { 
+                if (A == B && A == C)
+                {
+                    MessageBox.Show("Equilátero");
+                }
+                else if (A == B || B == C)
+                {
+                    MessageBox.Show("Isóceles");
+                }
+                else
+                {
+                    MessageBox.Show("Escaleno");
+                }
+            }
+            else
+                MessageBox.Show("Não é triângulo");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtA_Validated(object sender, EventArgs e)
+        {
+            double A = Convert.ToDouble(txtA.Text);
+            if (A == 0 || A < 0)
+                MessageBox.Show("Valor inválido");
+        }
+
+        private void txtB_Validated(object sender, EventArgs e)
+        {
+            double B = Convert.ToDouble(txtB.Text);
+            if (B == 0 || B < 0)
+                MessageBox.Show("Valor inválido");
+        }
+
+        private void txtC_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void txtC_Validated(object sender, EventArgs e)
+        {
+            double C = Convert.ToDouble(txtC.Text);
+            if (C == 0 || C < 0)
+                MessageBox.Show("Valor inválido");
+        }
+    }
+}
